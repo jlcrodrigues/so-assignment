51350272df68d3d02a3d38954a8c65dc7dec8217
andrebarbosa02 <82046536+andrebarbosa02@users.noreply.github.com>
2022-05-27 01:31:14 +0100

    Finished addmx.c
    Needs some cleanup on the code, other than that working and doing what it is supposed to do. Only doubt is the way it is printed, either this way or actually writing as it was a file on the stdout entry instead.

--- q2/addmx.c
+++ q2/addmx.c
@@ -3,14 +3,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
-#include "addmx.h"
+#include <sys/wait.h>
 
-struct matrixDimensions{
+typedef struct matrixInfo{
     int n;
     int m;
     int size;
-};
-matrixDimensions createMatrix (int * matrix, char* infile){
+    int * matrix;
+} matrixinfo;
+
+matrixinfo createMatrix (char* infile){
   FILE *fp;
   if((fp = fopen(infile,"r")) == NULL){
     perror("cannot open file");
@@ -19,16 +21,16 @@ matrixDimensions createMatrix (int * matrix, char* infile){
   size_t str_size = 0;
   char* str = NULL;
   getline(&str, &str_size, fp);
-  matrixDimensions dimensions;
-  dimensions.n = atoi(&str[0]);
-  dimensions.m = atoi(&str[2]);
-  dimensions.size = n*m;
-  matrix = (int*)malloc(dimensions.size*sizeof(int));
+  matrixinfo matrixinfo;
+  matrixinfo.n = atoi(&str[0]);
+  matrixinfo.m = atoi(&str[2]);
+  matrixinfo.size = matrixinfo.n*matrixinfo.m;
+  matrixinfo.matrix = (int*)malloc(matrixinfo.size*sizeof(int));
   int i = 0, j = 0;
   while (getline(&str, &str_size, fp) > 0 ) {
     char* token = strtok(str," ");
     while( token != NULL ) {
-      *(matrix+(i*dimensions.m)+j)=atoi(token);
+      *(matrixinfo.matrix+(i*matrixinfo.m)+j)=atoi(token);
       token=strtok(NULL," ");
       j++;
     }
@@ -36,57 +38,53 @@ matrixDimensions createMatrix (int * matrix, char* infile){
     j=0;
   }
   fclose(fp);
-  return dimensions;
+  return matrixinfo;
 }
 
-/*int readMatrix (int m, int i, int j, int value, int * partials){
-    return *(partials+(i-1)*m+j);
-}
-*/
 int main(int argc, char *argv[]) {
   
   char* infile1 = argv[1];
   char* infile2 = argv[2];
-  int * matrix1;
-  int * matrix2;
-  matrixDimensions dimensions1 = createMatrix(matrix1,infile1);
-  matrixDimensions dimensions2 = createMatrix(matrix2,infile2);
-  if (dimensions1.size!=dimensions2.size){
+  matrixinfo matrixinfo1= createMatrix(infile1);
+  matrixinfo matrixinfo2= createMatrix(infile2);
+  if (matrixinfo1.size!=matrixinfo2.size){
       perror("Matrix with different sizes");
       exit(EXIT_FAILURE);
   }
 
-  int *final_matrix = mmap(NULL, dimensions1.size*sizeof(int), PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS, 0, 0);
+  int *final_matrix = mmap(NULL, matrixinfo1.size*sizeof(int), PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS, 0, 0);
     if(final_matrix == MAP_FAILED){
         perror("mmap");
         exit(EXIT_FAILURE);
     }
-  for(int i = 0; i < dimensions1.size; i++) {
+  for(int i = 0; i < matrixinfo1.size; i++) {
       final_matrix[i] = 0;
   }
-  for(int j = 0; j < dimensions1.m; j++) {
+  for(int j = 0; j < matrixinfo1.m; j++) {
     pid_t pid;
     if ((pid = fork()) < 0) {
       perror("fork");
       exit(EXIT_FAILURE);
     }
     if(pid == 0) {
-      for(int i = 0; i < dimensions1.n ; i++){
-        *(final_matrix+(i*dimensions1.m)+j) = *(matrix1+(i*dimensions1.m)+j) + *(matrix2+(i*dimensions1.m)+j);
+      for(int i = 0; i < matrixinfo1.n ; i++){
+        int a = *(matrixinfo1.matrix+(i*matrixinfo1.m)+j);
+        int b = *(matrixinfo2.matrix+(i*matrixinfo2.m)+j);
+        *(final_matrix+(i*matrixinfo1.m)+j) = a + b;
       }
       exit(EXIT_SUCCESS);
     }
   }
-  for(i = 0; i < nprocs; i++) {
-    if ( waitpid(-1, NULL, 0) < 0) {
+  for(int i = 0; i < matrixinfo1.m; i++) {
+    if (waitpid(-1, NULL, 0) < 0) {
       perror("waitpid");
       exit(EXIT_FAILURE);
     }
   }
-  printf("%dx%d\n",dimensions1.n,dimensions1.m);
-  for (int i=0; i < dimensions1.n;i++){
-    for(int j=0; j<dimensions1.m;j++){
-      printf("%d ", *(final_matrix+(i*dimensions1.m)+j);
+  printf("%dx%d\n",matrixinfo1.n,matrixinfo1.m);
+  for (int i=0; i < matrixinfo1.n;i++){
+    for(int j=0; j<matrixinfo1.m;j++){
+      printf("%d ", *(final_matrix+(i*matrixinfo1.m)+j));
     }
     printf("\n");
   }
